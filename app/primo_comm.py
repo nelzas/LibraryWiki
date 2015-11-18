2bb96679cefab34cd70fa67ab2bfcdbd97d3500f
Adir Shemesh <adirsms@gmail.com>
2015-11-18 18:27:27 +0200

    added __len__ to iterator

--- app/primo_comm.py
+++ app/primo_comm.py
@@ -52,26 +52,26 @@ class Results:
 
     def __next__(self):
         if self.index < self.count:
-            res = self.results[self.index]
+            try:
+                res = self.results[self.index]
+            except IndexError:
+                raise StopIteration
             self.index += 1
             return res
         self.page += 1
         self.index = 0
-        try:
-            self.results = self._get_results()
-        except StopIteration:
-            raise StopIteration
+        self.results = self._get_results()
         return self.__next__()
 
     def _get_results(self):
         return [item['PrimoNMBib']['record']['control']['recordid'] for item in
                 self._search()['sear:SEGMENTS']['sear:JAGROOT']['sear:RESULT']['sear:DOCSET']['sear:DOC']]
 
+    def __len__(self):
+        return int(self._search()['sear:SEGMENTS']['sear:JAGROOT']['sear:RESULT']['sear:DOCSET']['@TOTALHITS'])
+
     def _search(self):
         res = get(Results._SEARCH_URL.format(self.query, 1 + (self.page - 1) * self.count, self.count))
         if res.status_code == 500:
             raise StopIteration()
         return res.json()
-
-
-
