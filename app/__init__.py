d8ebea90e880464c0413d1fd122faad0fd35880c
Adir Shemesh <adirsms@gmail.com>
2015-11-25 12:43:09 +0200

    added type dict

---
+++ app/__init__.py
@@ -0,0 +1,4 @@
+type_dict = {'dissertation', 'manuscript', 'image', 'lyrics', 'archive', 'reccomm', 'score_and_lyrics', 'article', 'book', 'recethno', 'audio', 'score_manuscript', 'ebook', 'journal'}
+
+def f():
+    pass
