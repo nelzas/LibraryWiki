1583568b97841a55e0879023368efc446ea352cc
Adir Shemesh <adirsms@gmail.com>
2015-12-23 11:50:44 +0200

    added authorities prototype. need refactoring and changes.

--- app/authorities.py
+++ app/authorities.py
@@ -1,3 +1,27 @@
+# d = {'100': 'person_name',
+#      '151': 'location_name',
+#      '400': 'alternate_person_name',
+#      '451': 'alternate_location_name',
+#      '046': 'life_span',
+#      '370': 'birth_death_places',
+#      '371': 'address',
+#      '372': 'related_term',
+#      '373': 'related_term',
+#      '376': 'related_term',
+#      '378': 'related_term',
+#      '374': 'occupation',
+#      '375': 'gender',
+#      '377': 'language',
+#      '500': 'relationship',
+#      '510': 'relationship',
+#      '550': 'relationship',
+#      '670': 'notes',
+#      '678': 'notes',
+#      '680': 'notes',
+#      '681': 'biography',
+#      }
+
+
 import xmltodict
 import json
 
@@ -18,14 +42,91 @@ def get_authorities():
                 buffer = ''
 
 
-for record, _ in zip(get_authorities(), range(50)):
-    # print(data for data in record['datafield'] if data['@tag'] == 100 and )
-    print(record['datafield'][0]['subfield'])
-    # print(code for code in )
-exit()
-for record, _ in zip(get_authorities(), range(50)):
-    tags = [data['@tag'] for data in record['datafield']]
-    if '100' in tags:
-        print("person id: " + record['controlfield'][2]['#text'])
-    elif '151' in tags:
-        print("location id: " + record['controlfield'][2]['#text'])
+codes = {
+    '100': 'person_name',
+    '151': 'location_name',
+    '046': 'life_span',
+    '371': 'address',
+    '374': 'occupation',
+    '375': 'gender',
+    '377': 'language',
+    '681': 'biography',
+}
+
+
+def parse_lang(tag, subfields):
+    return "{}_{}".format(tag, subfields['9']), subfields['a']
+
+
+def parse_dates(subfields):
+    dates = []
+    context = ('from ', 'until ')
+    state = 0
+    if subfields.get('s'):
+        dates.append(subfields.get('s'))
+    if subfields.get('t'):
+        dates.append(subfields.get('t'))
+        state = 1
+    if subfields.get('f'):
+        dates.append(subfields.get('f'))
+    if subfields.get('g'):
+        dates.append(subfields.get('g'))
+        state = 1
+    if len(dates) == 1:
+        return context[state] + dates[0]
+    return dates[0] + "-" + dates[1]
+
+
+def parse_address(subfields):
+    first = True
+
+    def no_none(param):
+        nonlocal first
+        addr = subfields.get(param) or ''
+        if first:
+            if addr:
+                first = False
+            return addr
+        if addr:
+            return ', ' + addr
+        return addr
+
+    return no_none('a') + no_none('b') + no_none('c')
+
+
+def parse_tag(tag, subfields):
+    if tag == 'person_name' or tag == 'location_name':
+        return parse_lang(tag, subfields)
+    if tag == 'life_span':
+        return tag, parse_dates(subfields)
+    if tag == 'address':
+        return tag, parse_address(subfields)
+    if tag == 'occupation' or tag == 'gender' or tag == 'language' or tag == 'biography':
+        return tag, subfields['a']
+
+
+def db_auth():
+    for record in get_authorities():
+        properties = {'id': record['controlfield'][2]['#text'], 'data': json.dumps(record)}
+        if record.get('datafield'):
+            dat = record['datafield'] if type(record['datafield']) is list else [record['datafield']]
+            tags = [codes.get(data['@tag']) or data['@tag'] for data in dat]
+            if codes['100'] in tags:
+                properties['type'] = 'person'
+            elif codes['151'] in tags:
+                properties['type'] = 'location'
+            else:
+                continue
+            if tags and dat[0].get('subfield'):
+                for i, tag in enumerate(tags):
+                    lstsub = dat[i].get('subfield')
+                    lstsub = lstsub if type(lstsub) is list else [lstsub]
+                    subfields = {sub['@code']: sub['#text'] for sub in lstsub}
+                    info = parse_tag(tags[i], subfields)
+                    if info:
+                        properties[info[0]] = info[1]
+                yield properties
+
+
+for a in db_auth():
+    print(a)
--- app/db_comm.py
+++ app/db_comm.py
@@ -1,11 +1,25 @@
 import py2neo
 from app import primo_comm
+from app import authorities
 
-py2neo.authenticate("localhost:7474", "neo4j", "123456")
-graph = py2neo.Graph()
-# graph.schema.create_uniqueness_constraint("Record", "recordid")
-for result, _ in zip(primo_comm.Results("בן גוריון", 30), range(40)):
-    properties = {'recordid': result['control']['recordid'], 'data': str(result)}
-    m = graph.merge_one("Record", "recordid", properties['recordid'])
-    m.properties.update(**properties)
-    m.push()
+py2neo.authenticate("104.155.27.166:7474", "neo4j", "R99R")
+graph = py2neo.Graph("http://104.155.27.166:7474/db/data/")
+
+
+def set_records():
+    # graph.schema.create_uniqueness_constraint("Record", "recordid")
+    for result, _ in zip(primo_comm.Results("בן גוריון", 30), range(40)):
+        properties = {'recordid': result['control']['recordid'], 'data': str(result)}
+        m = graph.merge_one("Record", "recordid", properties['recordid'])
+        m.properties.update(**properties)
+        m.push()
+
+
+def set_authorities():
+    # graph.schema.create_uniqueness_constraint("Authority", "id")
+    for authority, _ in zip(authorities.db_auth(), range(100)):
+        m = graph.merge_one("Authority", "id", authority["id"])
+        m.properties.update(**authority)
+        m.push()
+
+set_authorities()
