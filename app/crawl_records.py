7d40e973c74891f66b31080fd6382f5b9e37c6a2
Shai Benjamin <sheyeh@gmail.com>
2016-01-08 17:48:56 +0200

    crawler for records

---
+++ app/crawl_records.py
@@ -0,0 +1,26 @@
+from app.settings import *
+from app.pages import create_page_from_dictionary
+from app.personality import create_page_from_node
+import py2neo
+import json
+
+py2neo.authenticate(NEO4J_URL, NEO4J_USER, NEO4J_PASSWORD)
+graph = py2neo.Graph('http://' + NEO4J_URL + NEO4J_GRAPH)
+
+PAGE_SIZE = 500
+page_number = 0
+record_count = 0
+while True:
+    skip = PAGE_SIZE * page_number
+    records = graph.cypher.execute('match (r:Record) return r skip {} limit {}'.format(skip, PAGE_SIZE))
+    if not len(records):
+        break
+    page_number += 1
+    for nodes in records:
+        for record in nodes:
+            record_count += 1
+            record_data = eval(record['data'])
+            print(str("{}. {}".format(record_count, record_data['control']['recordid'])))
+            create_page_from_dictionary(record_data)
+
+print("Done. %s records processed" % record_count)
