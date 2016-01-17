0648a9d938a4965e84c722aeed58d8842c276fb5
Adir Shemesh <adirsms@gmail.com>
2016-01-17 23:51:10 +0200

    weizman

--- add_records.py
+++ add_records.py
@@ -1,5 +1,5 @@
 from app.db_comm import set_entities, create_records_authorities_relationships
 from app.entity_iterators import Results
 
-set_entities(Results('עגנון, שמואל יוסף', count=500))
+set_entities(Results('ויצמן, חיים', count=500))
 # create_records_authorities_relationships()
