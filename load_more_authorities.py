4a069ef7c7da29fda50b30ba5ce25d363b734af8
Adir Shemesh <adirsms@gmail.com>
2016-01-17 15:14:22 +0200

    shortened batch size even more

--- load_more_authorities.py
+++ load_more_authorities.py
@@ -8,7 +8,7 @@ def to_node(authority):
     return py2neo.Node(*authority.labels, **authority.properties)
 
 
-for i in range(300):
+for i in range(3000):
     graph.create(
-        *[to_node(authority) for authority in get_authorities(from_id=200000 + 100000 * i, to_id=210000 + 100000 * i)])
+        *[to_node(authority) for authority in get_authorities(from_id=200000 + 1000 * i, to_id=201000 + 1000 * i)])
     print('yo')
