d560e532acd07d99d2973dc50deaedaf48436273
Adir Shemesh <adirsms@gmail.com>
2016-01-17 23:46:31 +0200

    script for adding golda records

--- add_records.py
+++ add_records.py
@@ -1,5 +1,5 @@
 from app.db_comm import set_entities, create_records_authorities_relationships
 from app.entity_iterators import Results
 
-set_entities(Results('NNL_ALEPH'))
-create_records_authorities_relationships()
+set_entities(Results('גולדה מאיר', count=500))
+# create_records_authorities_relationships()
