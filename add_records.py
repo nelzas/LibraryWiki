46b1217eca5b135be2f180a4d6fbf9867b4c095d
Adir Shemesh <adirsms@gmail.com>
2016-01-18 21:53:01 +0200

    golda records

--- add_records.py
+++ add_records.py
@@ -1,4 +1,4 @@
 from app.db_comm import set_entities, create_records_authorities_relationships
 from app.entity_iterators import Results, Photos
 
-set_entities(Results('חיים נחמן ביאליק', count=500))
+set_entities(Results('גולדה מאיר', count=500))
