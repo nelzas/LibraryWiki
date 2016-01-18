9313a25bf40be93a6ff7ebd60a85da8587260314
Adir Shemesh <adirsms@gmail.com>
2016-01-18 21:54:24 +0200

    agnon records

--- add_records.py
+++ add_records.py
@@ -1,4 +1,4 @@
 from app.db_comm import set_entities, create_records_authorities_relationships
 from app.entity_iterators import Results, Photos
 
-set_entities(Results('גולדה מאיר', count=500))
+set_entities(Results('שמואל יוסף עגנון', count=500))
