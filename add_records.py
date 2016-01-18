5587313548b3a77eb95a538a3bcddab19dd9639c
Adir Shemesh <adirsms@gmail.com>
2016-01-18 20:50:10 +0200

    added debug print

--- add_records.py
+++ add_records.py
@@ -2,4 +2,5 @@ from app.db_comm import set_entities, create_records_authorities_relationships
 from app.entity_iterators import Results, Photos
 
 set_entities(Photos('חיים נחמן ביאליק', count=10))
+print('bam')
 create_records_authorities_relationships()
