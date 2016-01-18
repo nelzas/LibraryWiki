95e335bba90d7cf1ff1c310c314a6178a7a52368
Adir Shemesh <adirsms@gmail.com>
2016-01-18 20:48:26 +0200

    photo experiment

--- add_records.py
+++ add_records.py
@@ -1,5 +1,5 @@
 from app.db_comm import set_entities, create_records_authorities_relationships
-from app.entity_iterators import Results
+from app.entity_iterators import Results, Photos
 
-# set_entities(Results('חיים נחמן ביאליק', count=500))
+set_entities(Photos('חיים נחמן ביאליק', count=10))
 # create_records_authorities_relationships()
