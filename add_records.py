be71b88903bc86f019a25d7c072bd934ea018e51
Adir Shemesh <adirsms@gmail.com>
2016-01-18 21:09:34 +0200

    golda photos

--- add_records.py
+++ add_records.py
@@ -1,6 +1,6 @@
 from app.db_comm import set_entities, create_records_authorities_relationships
 from app.entity_iterators import Results, Photos
 
-set_entities(Photos('חיים נחמן ביאליק', count=10))
+set_entities(Photos('גולדה מאיר', count=10))
 print('bam')
 create_records_authorities_relationships()
