b2c7c3011d6cc44b805589a67e78e93aa6fb81eb
Adir Shemesh <adirsms@gmail.com>
2016-01-19 10:20:15 +0200

    fixed a typo

--- app/db_comm.py
+++ app/db_comm.py
@@ -58,7 +58,7 @@ def set_photos():
 def authority_photos(authority):
     query = authority.name
     photos = Photos(query)
-    for photo in zip(photos, range(10)):
+    for photo, _ in zip(photos, range(10)):
         portrait_node = create_entity(photo)
         # graph.create_unique(py2neo.Relationship(authority.node, "subject_of", portrait_node))
 
