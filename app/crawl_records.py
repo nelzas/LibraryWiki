a67526a4aad6f4e5fc45edd6f36f95ea38af472f
Adir Shemesh <adirsms@gmail.com>
2016-01-19 10:37:00 +0200

    added 'picture' in record type dict

--- app/crawl_records.py
+++ app/crawl_records.py
@@ -12,8 +12,8 @@ page_number = 0
 record_count = 0
 while True:
     skip = PAGE_SIZE * page_number
-    # records = graph.cypher.execute('match (p:Person)-[]-(r) with p, r, count(r) as rels where exists(p.person_name_heb) and rels > 0 return r skip {} limit {}'.format(skip, PAGE_SIZE))
-    records = graph.cypher.execute('match (p:Person)-[]-(r) with p, r, count(r) as rels where rels > 0 return r skip {} limit {}'.format(skip, PAGE_SIZE))
+    records = graph.cypher.execute('match (p:Person)-[]-(r) with p, r, count(r) as rels where exists(p.person_name_heb) and rels > 0 return r skip {} limit {}'.format(skip, PAGE_SIZE))
+    # records = graph.cypher.execute('match (p:Person)-[]-(r) with p, r, count(r) as rels where rels > 0 return r skip {} limit {}'.format(skip, PAGE_SIZE))
     # records = graph.cypher.execute('match (r:Person) return r skip {} limit {}'.format(skip, PAGE_SIZE))
     if not len(records):
         break
--- app/settings.py
+++ app/settings.py
@@ -3,7 +3,7 @@ WIKI_PATH = '/'
 WIKI_USER = "hackibot"
 WIKI_PASSWORD = "hackita02"
 
-NEO4J_URL = 'localhost:7474'
+NEO4J_URL = 'localhost:7777'
 NEO4J_USER = 'neo4j'
 NEO4J_PASSWORD = 'R99R'
 NEO4J_GRAPH = "/db/data/"
