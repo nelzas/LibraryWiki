4e6cd7b5ef29510fe6c41f71394347ef6d54a177
Adir Shemesh <adirsms@gmail.com>
2016-01-19 09:59:41 +0200

    just checking

--- app/db_comm.py
+++ app/db_comm.py
@@ -11,6 +11,8 @@ graph = py2neo.Graph('http://' + NEO4J_URL + NEO4J_GRAPH)
 
 
 def get_entity_node(entity):
+    print(entity)
+    exit()
     return graph.merge_one(entity.labels[0], "id", entity.properties["id"])
 
 
