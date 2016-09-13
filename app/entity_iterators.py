0105b245ce33d925c0afbf545d3e9d9b36bcb3e9
Shai Benjamin <sheyeh@gmail.com>
2016-09-13 22:02:56 +0300

    another case of use PRIMO constant instead of hardcoded address in url

--- app/entity_iterators.py
+++ app/entity_iterators.py
@@ -161,7 +161,7 @@ class Portraits(Photos):
 
     @property
     def _search_url(self):
-        return 'http://primo.nli.org.il/PrimoWebServices/xservice/search/brief?institution=NNL' \
+        return 'http://' + PRIMO + '/PrimoWebServices/xservice/search/brief?institution=NNL' \
                '&loc=local,scope:(NNL01_Schwad)&query=facet_topic,exact,"{} Y M–Portraits"&sortField=&indx={}' \
                '&bulkSize={}&json=true'
 
